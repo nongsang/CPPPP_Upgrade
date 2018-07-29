edb6d9895bc8264a29c589106b46141419b68110
nongsang <shdtkd37@gmail.com>
2018-07-30 03:39:16 +0900

    7월 30일 배열의 for문과 foreach문 비교

--- 02.Array_For_Foreach_Comparison.cs
+++ 02.Array_For_Foreach_Comparison.cs
@@ -8,7 +8,7 @@ using System.Diagnostics;       // 성능측정하기 위한 Stopwatch를 쓰려
 
 namespace CPPPP
 {
-    class For_Foreach_Array_Performance
+    class Array_For_Foreach_Comparison
     {
         public static void Main()
         {
@@ -39,6 +39,14 @@ namespace CPPPP
             sw.Stop();                      // 스탑워치를 끈다.
 
             WriteLine("소요시간 : " + sw.Elapsed);      // 배열에서는 foreach문이 for문보다 2배정도 더 빠르다.
+
+            // 처리 대상에 따라서 성능차가 많이 날 수 있다.
+            // for문은 특정 조건이 성립할 때까지 반복한다.
+            // 거기에 인덱스를 하나씩 증가시키면서 인덱스에 해당하는 요소를 가져온다.
+            // 'for문 + 인덱스'의 오버헤드가 발생하게 된다.
+            // 배열처럼 임의접근(Random Access, 인덱스로 아무곳이나 찍어서 접근하는 것)이 가능한 컬렉션은 성능에 큰 문제는 없다.
+            // 하지만 list같은 임의접근이 불가능하고 순차접근(Sequential Access)이 필요한 컬렉션은 foreach문이 더 적합하다.
+            // foreach문은 특정 열거 인터페이스가 열거된 요소를 하나씩 다음으로 넘어가면서 요소를 가져온다.
         }
     }
 }
