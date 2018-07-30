2a992648a08c6d23613cc629cd4ece64f28de284
nongsang <shdtkd37@gmail.com>
2018-07-30 21:44:39 +0900

    7월 30일 람다와 배열에 의한 메모리 부족

--- 10.Lamda_Array_Out_Of_Memory.cs
+++ 10.Lamda_Array_Out_Of_Memory.cs
@@ -17,13 +17,17 @@ class SimpleSum
         {
             return array.Sum(); // 배열값의 합을 반환한다.
                                 // 이번에는 array와 람다때문에 문제가 발생한다.
+                                // array는 배열형이며 Sum()은 IEnumerable형이다.
+                                // 따라서 배열을 열거형으로 순차적으로 반환을 하므로 속도가 오래걸린다.
+                                // 속도가 오래걸리는 바람에 아직 람다의 작업이 채 끝나기도 전에 array의 데이터는 10만번이나 쌓이게 된다.
+                                // 그렇기 때문에 메모리 부족이 일어난다.
         };
     }
 }
 
 namespace CPPPP
 {
-    class Array_Out_Of_Memory
+    class Lamda_Array_Out_Of_Memory
     {
         public static void Main()
         {
