c5d311948c911ddf7894543d0d4978c3286a0a7b
nongsang <shdtkd37@gmail.com>
2018-07-30 23:08:53 +0900

    7월 30일 형변환의 복잡성

--- 12.Type_Casting_Complex.cs
+++ 12.Type_Casting_Complex.cs
@@ -23,9 +23,9 @@ namespace CPPPP
             Base[] array = { new Base(), new Extended() };
 
             foreach (var item in array)     // array에 있는 요소들 중에
-                if (item is Base)           // Base의 상속을 받은 놈들은
+                if (item is Extended)           // Extended이거나 상속을 받은 놈들은
                     ((Extended)item).SayIt();   // Etended로 형변환하고 SayIt()를 실행
-                                                //근데 ()가 많아서 헷깔리는데 바꿀 방법이 없을까?
+                                                // 근데 ()가 많아서 헷깔리는데 바꿀 방법이 없을까?
         }
     }
 }
